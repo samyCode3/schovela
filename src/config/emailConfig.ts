0b20c6dbc4eddc160386044467ca48af63c289ec
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-04-21 11:50:10 +0100

    Made some changed

--- src/config/emailConfig.ts
+++ src/config/emailConfig.ts
@@ -2,10 +2,12 @@ import config from './default';
 // import * as config from 'config';
 import { createTransport } from 'nodemailer';
 
+console.log(config.mailgun);
+
 export const mail = createTransport({
     host: config.mailgun.HOST,
     port: Number(config.mailgun.PORT),
-    secure: false,
+    secure: true,
     auth: {
         user: config.mailgun.USER,
         pass: config.mailgun.PASS
--- src/utils/sendEmail.ts
+++ src/utils/sendEmail.ts
@@ -3,7 +3,7 @@ import {
  StatusCodes
 } from 'http-status-codes'
 import { ApiResponseType } from "../interface/api.interface";
-export const sendEmail = async (to, subject, text, html):  Promise<ApiResponseType> => {
+export const sendEmail = async (to, subject, text, html):  Promise<void> => {
   try {
   const send = await mail.sendMail({
     from: `${process.env.SMTP_NAME}\n ${process.env.SMTP_EMAIL}`,
@@ -12,8 +12,9 @@ export const sendEmail = async (to, subject, text, html):  Promise<ApiResponseTy
     text,
     html,
   });
-   return { ok : true, status: StatusCodes.OK, message: send.messageId}
+   console.log({ ok : true, status: StatusCodes.OK, message: send.messageId})
   } catch (error) {
+    console.log(error);
     throw error;
   }
 };
