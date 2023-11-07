b02225773111fd6ebca3bc3ba7c02e11cd5485e0
Femi Fatokun <76601789+talibackend@users.noreply.github.com>
2023-11-07 15:47:39 +0100

    Removed multer

--- src/routes/dashboard.routes.ts
+++ src/routes/dashboard.routes.ts
@@ -1,11 +1,10 @@
 import {Router} from 'express'
 import { UploadProfileController, getProfileImageController, deleteProfileController } from '../controller/index.controller'
 import { AuthUser } from '../middleware/auth'
-import { upload } from '../middleware/multer'
 
 export const dashboardRoutes =  Router()
 
-dashboardRoutes.post('/image/upload', upload.single('profile_img'), AuthUser,  UploadProfileController) 
+dashboardRoutes.post('/image/upload', AuthUser,  UploadProfileController) 
 dashboardRoutes.get('/image', AuthUser, getProfileImageController)
 dashboardRoutes.delete('/image', AuthUser, deleteProfileController)
  
