e5aaf2a8b5533a033060ef4ae449e3cc4bce81fc
samson onifade <gbemilekeadeboyega@gmail.com>
2023-12-07 05:47:06 +0000

    total download added

--- src/services/analytic.service.ts
+++ src/services/analytic.service.ts
@@ -5,15 +5,17 @@ import { UserModel } from "../model/user.model"
 import { ViewModel } from "../model/view.model"
 import { StatusCodes } from "http-status-codes"
 import { ROLE } from "../interface/user.interface"
+import { DownloadModel } from "../model/download.model"
 
 export const Analytics = async (): Promise<ApiResponseType> => {
     let total_user = await UserModel.count({ where: {[Op.not] : [{role: ROLE.admin}]}})
     let total_content = await PostModel.count()
     let total_views = await ViewModel.count()
+    let downloads = await DownloadModel.count()
     return {
         ok: true,
         status: StatusCodes.OK,
         message: `App Analytics`,
-        body : {total_user, total_content, total_views}
+        body : {total_user, total_content, total_views, downloads}
     }
 }
